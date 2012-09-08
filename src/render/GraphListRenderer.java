89c2c4df79e9e82b7c52f1f342f2d4a753aff054
maclean <gilleain.torrance@gmail.com>
2012-09-08 18:22:31 +0100

    Missed some deleted files, and use the ListAWTPainter

--- src/render/GraphListRenderer.java
+++ src/render/GraphListRenderer.java
@@ -7,15 +7,15 @@ import java.util.List;
 import javax.vecmath.Point2d;
 
 import layout.GraphLayout;
-import listlayout.CanvasLayout;
-import listlayout.GridCanvasLayout;
+import layout.ILayout;
+import layout.LinearLayout;
 import model.Graph;
 import sketch.GraphListSketcher;
 import sketch.GraphSketcher;
 import sketcher.Sketcher;
-import awt.BasicAWTPainter;
-import awt.CompositeAWTPainter;
+import awt.ListAWTPainter;
 import diagram.element.IDiagramElement;
+import divide.LinearDivider;
 
 
 /**
@@ -26,29 +26,31 @@ import diagram.element.IDiagramElement;
  */
 public class GraphListRenderer  {
 	
-	private CanvasLayout canvasLayout;
+	private ILayout canvasLayout;
 	
 	private Sketcher<List<Graph>, List<IDiagramElement>> listSketcher;
 	
-	private CompositeAWTPainter painter;
+	private ListAWTPainter painter;
 	
 	public GraphListRenderer(Graphics graphics) {
-		this(new GridCanvasLayout(), graphics);
+		this(new LinearLayout(), graphics);
 	}
 	
-	public GraphListRenderer(CanvasLayout canvasLayout, Graphics graphics) {
+	public GraphListRenderer(ILayout canvasLayout, Graphics graphics) {
 		this.canvasLayout = canvasLayout;
 		this.listSketcher = new GraphListSketcher(new GraphSketcher(new GraphLayout()));
-		this.painter = new CompositeAWTPainter(graphics);
+		this.painter = new ListAWTPainter(graphics);
 	}
 	
 	public void render(List<Graph> graphs, Rectangle2D canvas) {
 		List<IDiagramElement> diagrams = listSketcher.sketch(graphs);
+		// XXX divider shouldn't be created here, but in the constructor
+		painter.paint(diagrams, canvas, new LinearDivider(diagrams.size()));	
 	}
 
 	public void render(List<Graph> graphs, Point2d center) {
 		List<IDiagramElement> diagrams = listSketcher.sketch(graphs);
-		
+		painter.paint(diagrams, center, canvasLayout);
 	}
 
 }
